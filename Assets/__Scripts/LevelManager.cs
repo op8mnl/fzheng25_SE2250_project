f0c1e29210a408d0f9c28cb4ae4b6ee6f18b6b1c
Felix Zheng <zheng.felix@outlook.com>
2022-02-10 17:14:14 -0500

    Changes

--- Assets/__Scripts/LevelManager.cs
+++ Assets/__Scripts/LevelManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelManager
+public class LevelManager : MonoBehaviour
 {
     public void getNextLevel(int currentLevel, string direction)
     {
--- Assets/__Scripts/TextController.cs
+++ Assets/__Scripts/TextController.cs
@@ -15,26 +15,14 @@ public class TextController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D rectangle)
     {
 
-        if (rectangle.CompareTag("plot1"))
+        if (rectangle.CompareTag("Player"))
         {
             plot.text = "Young Scott is on a life changing journey to find love on Timber. " +
                "Scott’s dedication to his journey led him to have his phone " +
                "confiscated during Ari Steinfeld’s MATH 1512 lecture.";
 
         }
-        else if (rectangle.CompareTag("plot2"))
-        {
-            plot.text = "Young Scott is on a life changing journey to find love on Timber. " +
-               "Scott’s dedication to his journey led him to have his phone " +
-               "confiscated during Ari Steinfeld’s MATH 1512 lecture.";
-        }
-        else if (rectangle.CompareTag("plot3"))
-        {
-            plot.text = "Young Scott is on a life changing journey to find love on Timber. " +
-               "Scott’s dedication to his journey led him to have his phone " +
-               "confiscated during Ari Steinfeld’s MATH 1512 lecture.";
-        }
-
+ 
     }
     private void OnTriggerExit2D(Collider2D rectangle)
     {
