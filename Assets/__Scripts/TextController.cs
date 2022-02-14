501e6c03d791159d976dc2931c713fd4d5537054
Felix Zheng <zheng.felix@outlook.com>
2022-02-13 20:25:54 -0500

    level 0 overhaul

--- Assets/__Scripts/TextController.cs
+++ Assets/__Scripts/TextController.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TextController : MonoBehaviour
 {
-    public Text plot;
+    public TMP_Text plot;
 
     void Start()
     {
