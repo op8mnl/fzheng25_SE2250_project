99e17e86eb46d54df19f80e2eb9f6d0aeba43df8
Tristan Jin <tjin368@gmail.com>
2022-03-20 23:37:30 -0400

    Enemy ai following player completed

--- Assets/__Scripts/EnemyGFX.cs
+++ Assets/__Scripts/EnemyGFX.cs
@@ -6,15 +6,14 @@ using Pathfinding;
 public class EnemyGFX : MonoBehaviour
 {
     public AIPath aiPath;
-    public GameObject go;
 
     // Update is called once per frame
     void Update()
     {
         if (aiPath.desiredVelocity.x >= 0.1f) { // moving right
-            go.transform.rotation = Quaternion.Euler(0,0,0);
+            transform.rotation = Quaternion.Euler(0,0,0);
         } else if (aiPath.desiredVelocity.x <= -0.1f) { // moving left
-            go.transform.rotation = Quaternion.Euler(0,180,0);
+            transform.rotation = Quaternion.Euler(0,180,0);
         }
     }
 }
