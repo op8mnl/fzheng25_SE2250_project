e9f2e114f2cc95c2fda3cfdbcbf11e444d186d26
Tristan Jin <tjin368@gmail.com>
2022-04-03 17:26:05 -0400

    removed unused assets and changed small bird drop rate to being random

--- Assets/__Scripts/BirdController.cs
+++ Assets/__Scripts/BirdController.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class BirdController : Enemy
 {
-    // private Transform scott;
     public float maxDistance;
     public float minDistance;
     public bool canFly;
@@ -13,36 +12,16 @@ public class BirdController : Enemy
 
     public GameObject smallBirdPrefab;
 
-    // Animator birdAnim; //animator
-
     Rigidbody2D rb;
     private float nextDrop;
 
     void Start()
     {
-        // scott = GameObject.FindGameObjectWithTag("Player").transform;
-        // birdAnim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         nextDrop = Random.Range (0.2f, 2f);
     }
  
     void FixedUpdate() {
-        // if (transform.position.x > scottPlayer.transform.position.x) {
-        //     // face left
-        //     transform.localScale = new Vector3(isRightFacing ? -1 : 1, 1, 1);
-
-        //     moveDir = new Vector2(-speed, 0f);   // move to the left if needed, and if on a hill, move accordingly
-        //     // Debug.Log("moveDir: " + moveDir);
-        // } else {
-        //     // face right
-        //     transform.localScale = new Vector3(isRightFacing ? 1 : -1, 1, 1);
-
-        //     moveDir = new Vector2(speed, 0f);    // move to the right if needed, and if on a hill, move accordingly
-        //     // Debug.Log("moveDir: " + moveDir);
-        // }
-
-
-
         if (speed < 0) {
             // face left
             transform.localScale = new Vector3(isRightFacing ? -1 : 1, 1, 1);
@@ -53,21 +32,14 @@ public class BirdController : Enemy
 
         rb.velocity = new Vector2(speed, 0f);;    // force that will move the enemy in the desired direction
 
-        // if (Vector3.Distance(transform.position, scottPlayer.transform.position) >= minDistance) {
-        //     rb.velocity = moveDir;
-
-            // StartCoroutine(createSmallBird());
-
-            if (Vector3.Distance(transform.position, scottPlayer.transform.position) <= maxDistance)
-            {
-                // shoot out small birds at random intervals
-                // StartCoroutine(createSmallBird());
-                if (Time.time > nextDrop) {
-                    nextDrop = Time.time + Random.Range (0.2f, 2f);
-                    Instantiate(smallBirdPrefab, new Vector2(transform.position.x,transform.position.y - 0.5f), smallBirdPrefab.transform.rotation * Quaternion.Euler(0f, 0f, 0f));
-                }
+        if (Vector3.Distance(transform.position, scottPlayer.transform.position) <= maxDistance)
+        {
+            // shoot out small birds at random intervals
+            if (Time.time > nextDrop) {
+                nextDrop = Time.time + Random.Range (0.2f, 2f);
+                Instantiate(smallBirdPrefab, new Vector2(transform.position.x,transform.position.y - 0.5f), smallBirdPrefab.transform.rotation * Quaternion.Euler(0f, 0f, 0f));
             }
-        // }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other) {
@@ -79,13 +51,4 @@ public class BirdController : Enemy
     void OnTriggerExit2D(Collider2D other) {
         onTrigExit(other);
     }
-
-    // private IEnumerator createSmallBird()
-    // {
-    //     //Wait for 2 seconds
-    //     yield return new WaitForSeconds(2);
-
-    //     //create small bird from prefab
-    //     Instantiate(smallBirdPrefab, new Vector2(transform.position.x,transform.position.y - 0.5f), smallBirdPrefab.transform.rotation * Quaternion.Euler(0f, 0f, 0f));
-    // }
 }
--- Assets/__Scripts/SmallBirdController.cs
+++ Assets/__Scripts/SmallBirdController.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class SmallBirdController : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Hill"))
