d69d971248015567d4f3415b27365554761270bd
Felix Zheng <zheng.felix@outlook.com>
2022-04-04 18:57:23 -0400

    muller

--- Assets/__Scripts/MullerController.cs
+++ Assets/__Scripts/MullerController.cs
@@ -2,26 +2,122 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MullerController : MonoBehaviour
+public class MullerController : Enemy
 {
     private bool triggerFinalExam=false;
     private bool ultCooldown = false;
     private bool swingCooldown = false;
     public float cd = 15f;
     public float cds = 5f;
+    public float maxDistance;
+    public float minDistance;
     public Animator anim;
+    Rigidbody2D rb;
     public GameObject f;
+    private bool isOnGround = false;
+    public bool isRightFacing;
+    private float ySpeed = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         getShouldUltimate();
-        Attack();
+        //Attack();
+        
+    }
+    private void FixedUpdate()
+    {
+        Vector2 moveDir;    // force that will move the enemy in the desired direction
+
+        if (transform.position.x > scottPlayer.transform.position.x)
+        {
+            // face left
+            transform.localScale = new Vector3(isRightFacing ? -1 : 1, 1, 1);
+
+
+            if (!isOnGround)
+            {
+                ySpeed = -speed;
+            }
+            else
+            {
+                ySpeed = 0f;
+            }
+
+            moveDir = new Vector2(-speed, ySpeed);   // move to the left if needed, and if on a hill, move accordingly
+            
+            if (Vector3.Distance(transform.position, scottPlayer.transform.position) >= minDistance)
+            {
+                // only move if greater than minimum distance
+                rb.velocity = moveDir;
+                if (rb == null)
+                {
+                    rb = GetComponent<Rigidbody2D>();
+                }
+
+                if (Vector3.Distance(transform.position, scottPlayer.transform.position) <= maxDistance)
+                {
+                    //call functions like a shoot or swing function at here or something
+                    if (anim == null)
+                    {
+                        anim = GetComponent<Animator>();
+                    }
+
+                    if (!swingCooldown && !triggerFinalExam) 
+                    {
+
+                        anim.SetBool("isSwing", true);
+                        swingCooldown = true;
+                        Invoke("cooldownS", cds);
+
+                    }
+                    else
+                    {
+                        anim.SetBool("isSwing", false);
+                    }
+                    if (triggerFinalExam == true && !ultCooldown && !anim.GetBool("isSwing"))
+                    {
+
+                        ultCooldown = true;
+                        Invoke("shortUlt", 3f);
+                        Invoke("cooldownU", cd);
+                        Invoke("spawnF", 1.25f);
+                        Debug.Log("Ult");
+                        anim.SetBool("isUlt", true);
+                        GetComponent<Rigidbody2D>().AddForce(transform.up * 400);
+                        GetComponent<Rigidbody2D>().AddForce(transform.right * -400);
+
+                    }
+                    else
+                    {
+                        anim.SetBool("isUlt", false);
+                    }
+
+                }
+            }
+        }
+        else
+        {
+            // face right
+            transform.localScale = new Vector3(isRightFacing ? 1 : -1, 1, 1);
+
+            if (!isOnGround)
+            {
+                ySpeed = -speed;
+            }
+            else
+            {
+                ySpeed = 0f;
+            }
+
+            moveDir = new Vector2(speed, ySpeed);    // move to the right if needed        }
+        }
     }
     void getShouldUltimate()
     {
@@ -32,6 +128,7 @@ public class MullerController : MonoBehaviour
             triggerFinalExam = true;
         }
     }
+    /*
     void Attack()
     {
         if (triggerFinalExam == true && !ultCooldown && !anim.GetBool("isSwing"))
@@ -65,7 +162,7 @@ public class MullerController : MonoBehaviour
         }
 
     }
-
+    */
     void cooldownS()
     {
 
